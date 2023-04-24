a46a8129742b8f895de60721dbd08cc4f26dc2f3
gabziz <115720563+gabziz@users.noreply.github.com>
2023-04-24 11:26:56 -0300

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,25 @@
+//const light = document.getElementsByClassName("light")
+//const toggle = document.querySelector("#toggle")
+//const sunIcon = document.querySelector(".toggle .fa-sun")
+//const mooIcon = document.querySelector(".toggle .fa-moo")
+
+//toggle.addEventListener("change", () => {
+
+  //  body.classList.toggle("dark")
+//})
+
+//const toggleBtn = document.getElementById("chk")
+//const body = document.body
+//const header = document.getElementById("head")
+
+//toggleBtn.addEventListener("click", function() {
+  //  body.classList.toggle("dark-mode")
+    //header.classList.toggle("dark-mode-header")
+//} )
+
+//const chk = document.getElementById('chk')
+
+//chk.addEventListener('changer', () => {
+  //  document.body.classList.toggle('dark')
+//})
+
